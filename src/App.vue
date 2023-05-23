46b055255f9b4c3e071509f96543e8bc057ebd9c
Gustavo-DelColi <gustavodelcoli@hotmail.com>
2023-05-23 09:28:48 -0300

    Aula 08 - Ligacao de dados bidirecional (diretiva v model)

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,76 @@
 <template>
+  <div>
     <div>
-      <h1 :class="{'title':true, 'title-home': isHome}">
-        Curso Vue 3
-      </h1>
-      <p :class="pClass">
-        Sem encarar, sem voltar o rosto na direção da pessoa ou coisa para a qual se olha; de esguelha: Olhou-a de soslaio, disfarçadamente.
-      </p>
-      <p :style="styleClass">
-        Olhar de lado , contrário de secar ,que é encarar
-      </p>
-      
-      <div
-        v-for="(obj, index) in todos"
-        v-bind:key="obj.id"
-        class="todos-item"
-        >
-        <img
-          v-if="obj.imgSrc"
-          :src="obj.imgSrc"
-        >
-        {{ index }} - {{ obj.title }}
-        </div>
+      One-way data binding<br>
+      Two-way data binding<br>
+      v-model -> formulários
     </div>
+    <br><br>
+    <div>
+      <label>Nome</label><br>
+      <input
+        v-model="name"
+        type="text"
+        > <br>
+        {{ name }}
+    </div>
+    <br><br>
+    <div>
+      <label>Sports</label><br>
+      <select v-model="sports">
+        <option value="">Escolha</option>
+        <option value="futebol">Futebol</option>
+        <option value="skate">skate</option>
+        <option value="tenis">tenis</option>
+      </select><br>
+      {{ sports }}
+    </div>
+    <br><br>
+    <div>
+      <label> Newsletter</label><br>
+      <input
+        v-model="newsletter"
+        type="radio"
+        value="Sim"
+      > Sim
+
+      <input
+        v-model="newsletter"
+        type="radio"
+        value="Não"
+      > Não <br>
+      {{ newsletter }}
+
+    </div>
+    <br><br>
+    <div>
+      <label> Contrato</label><br>
+      <input
+        v-model="contract"
+        type="checkbox"
+        value="Sim"
+      > Aceita os termos <br>
+
+      {{ contract }}
+
+    </div>
+    <div>
+      <label> Colors </label><br>
+      <input
+        v-model="colors"
+        type="checkbox"
+        value="Blue"
+      > Blue
+
+      <input
+        v-model="colors"
+        type="checkbox"
+        value="Red"
+      > Red <br>
+      {{ colors }}
+
+    </div>
+  </div>
 </template>
 
 <script>
@@ -29,75 +78,19 @@ export default {
   name: 'App',
   data() {
     return {
-      isHome: false,
-      classVar: 'title',
-      pClass: ['text', 'text-home'],
-      styleClass: {'color': 'aqua','backgroundColor': 'red'},
-      todos:[
-            {
-              "userId": 1,
-              "id": 1,
-              "title": "delectus aut autem",
-              "completed": false,
-              "imgSrc":'https://fakeimg.pl/150?text=fasdasd',
-              "imgAlt": 'Foto de Jon Snow',
-            },
-            {
-              "userId": 1,
-              "id": 2,
-              "title": "quis ut nam facilis et officia qui",
-              "completed": false,
-              "imgSrc":'https://fakeimg.pl/150?text=fasdasd',
-            },
-            {
-              "userId": 1,
-              "id": 3,
-              "title": "fugiat veniam minus",
-              "completed": false
-            },
-            {
-              "userId": 1,
-              "id": 4,
-              "title": "et porro tempora",
-              "completed": true
-            },
-            {
-              "userId": 1,
-              "id": 5,
-              "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
-              "completed": false
-            }
-          ]
-      }
-
+      name:'Jon Snow',
+      sports:'',
+      newsletter:'',
+      contract: false,
+      colors:[]
     }
+
   }
+}
 
 </script>
 
 <style>
-.title{
-  font-size:  20px;
-  color: blue;
-}
-.title-home{
-  font-size: 40px;
-  color: green;
-}
-.text {
-  color: yellow;
-}
-.text-home{
-  color:#42b983;
-}
-.todos-item{
-  background: #000;
-  margin: 0 0 5px 0;
-  padding: 3px 6px;
-  color: #fff
-
-}
-
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
